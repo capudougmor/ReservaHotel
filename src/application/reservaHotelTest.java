6d6e10118116ebc0ec32778b4ced50cc2bfaf86e
Douglas Mendes <capudougmor@gmail.com>
2020-01-31 16:11:57 -0300

    Tratamento de excesoes com IllegalArgumentException

--- src/application/reservaHotelTest.java
+++ src/application/reservaHotelTest.java
@@ -1,10 +1,44 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import entities.Reservation;
+
 public class reservaHotelTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		try {
+			System.out.print("Room number: ");
+			int roomNumber = sc.nextInt();
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			Date checkIn = sdf.parse(sc.next());
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			Date checkOut = sdf.parse(sc.next());
+			
+			Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+			System.out.println("Resevation: " +reservation);
+			
+			System.out.println("Enter data to update the reservation: ");
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			checkIn = sdf.parse(sc.next());
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			checkOut = sdf.parse(sc.next());
+			
+			reservation.updateDates(checkIn, checkOut);
+			System.out.println("Resevation: " +reservation);
+		}catch (ParseException e) { 
+			System.out.println("invalid date format");
+		}catch (IllegalArgumentException e) {
+			System.out.println("Erro in reservation: " + e.getMessage());
+		}
+		sc.close();
 	}
 
 }
